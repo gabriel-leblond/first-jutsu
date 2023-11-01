cdf3226cc48b51cbef3b04d655442269cdc50004
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-11-01 17:46:01 +0100

    ADD - v-bind - props - input - v-model

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
-
+  import { ref } from 'vue'
+  import HelloWorld from '@/components/HelloWorld.vue'
+  // const subTitle = "Je suis le sous titre du parent"
+  // je crée une variable réactive avec ref
+  const name = ref("");
+  const age = ref("");
 </script>
 
 <template>
-
+  <!-- Je passe subTitle en props a l'enfant HelloWorld.vue -->
+  <!-- si j'acualise au dessus ma variable elle est maintenant -->
+  <!-- lié dynamiquement a l'enfant grâce au v-bind -->
+  <HelloWorld :name ="name" :age ="age"/>
+  <input type="text" v-model="name" />
+  <input type="text" v-model="age" />
 </template>
 
 <style>
