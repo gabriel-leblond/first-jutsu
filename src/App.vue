d9a4df364a33390230a97875244d5829f9f55d17
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-11-01 19:29:40 +0100

    ADD - computed()

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,28 @@
 <script setup>
-  import { ref } from 'vue'
-  // import d'un component enfant
+  import { computed, ref } from 'vue'
   import HelloWorld from '@/components/HelloWorld.vue'
 
-  const user = ref({
-    name: 'sushi',
-    age: 10,
+  const name = ref('Sushi');
+  const age = ref(34);
+  // Création d'une sentence, c'est une valeur calculer
+  // de cette maniere si age ou name change la sentence
+  // est actualisé. On utlisie computed.
+  const sentence = computed(() => {
+    return `Bonjour je suis ${name.value} et j'ai ${age.value}`;
   });
-
-  const display = () => {
-    console.log(user.value)
-  }
 
 </script>
 
 <template>
-  <HelloWorld>
-    <template v-slot:name>
-      <p>{{ user.name }}</p>
-    </template>
-    <template v-slot:age>
-      <p>{{ user.age }}</p>
-    </template>
-  </HelloWorld>
-  <div v-if="user.age > 15">
-    Vous êtes majeur !
-  </div>
-  <div v-else>
-    Vous êtes n'êtes pas majeur !
-  </div>
-
+  <HelloWorld/>
+  <span>{{ sentence }}</span>
 </template>
 
 <style>
-
+  .text-red {
+    color: red;
+  }
+  .text-green {
+    color: green;
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,7 @@
 </script>
 
 <template>
-  <h1> Hello Wolrd</h1>
-    <slot name="name"> </slot>
-    <slot name="age"> </slot>
+
 </template>
 
 <style>
